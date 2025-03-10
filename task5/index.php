e5d812ae0d1359141466868d29766178c9291d14
Mike Romanov <romanosir@yandex.ru>
2025-03-10 09:47:41 +0300

    com

--- task5/index.php
+++ task5/index.php
@@ -188,7 +188,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
   // Если нет предыдущих ошибок ввода, есть кука сессии, начали сессию и
   // ранее в сессию записан факт успешного логина.
-  if (empty($errors) && !empty($_COOKIE[session_name()]) &&
+  if ( !empty($_COOKIE[session_name()]) &&
       session_start() && !empty($_SESSION['login'])) {
         try {
             $stmt_select = $db->prepare("SELECT * FROM users WHERE id = ?");
