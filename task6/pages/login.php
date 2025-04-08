922d9727fe2a65c2b8ddeb8170ef6e0926b1d09c
Mike <romanosir@yandex.ru>
2025-04-08 21:51:13 +0300

    2

--- task6/pages/login.php
+++ task6/pages/login.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
@@ -23,25 +23,5 @@
     <a class="admin_ref" href="admin.php">Войти как администратор</a>
 
   </body>
-</html> -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="loginstyle.css">
-  <title>Вход</title>
-</head>
-<body>
-    <div id="form-menu" class="form-menu">
-        <form action="" method="post">
-            <p>Логин</p>
-            <input name="login" />
-            <p>Пароль</p>
-            <input name="pass" />
-            <input id="button" type="submit" value="Войти" />
-        </form>
-    </div>
-    
-</body>
-</html> 
+</html>
+
