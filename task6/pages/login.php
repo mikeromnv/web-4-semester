0558242991452a91bfd2b977d9a2ea351d671650
Mike <romanosir@yandex.ru>
2025-04-08 21:55:26 +0300

    st login

--- task6/pages/login.php
+++ task6/pages/login.php
@@ -3,25 +3,22 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"  href="./styles/style.css">
+    <link rel="stylesheet"  href="loginstyle.css">
     <title> LAB6 </title>
   </head>
   <body>
 
-    <form class="login_form" action="" method="post">
-      <label> 
-        Логин <br/>
-        <input name="login" />
-      </label> <br/>
-      <label> 
-        Пароль <br/>
-        <input name="password" />
-      </label> <br/>
-      <input class="login_button" type="submit" value="Войти" />
-    </form>
+    <div id="form-menu" class="form-menu">
+        <form action="" method="post">
+            <p>Логин</p>
+            <input name="login" />
+            <p>Пароль</p>
+            <input name="pass" />
+            <input id="button" type="submit" value="Войти" />
+        </form>
+    </div>
 
     <a class="admin_ref" href="admin.php">Войти как администратор</a>
 
   </body>
 </html>
-
