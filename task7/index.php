7519e05eff75f2162fd08454a4f13518a216eabd
Mike <romanosir@yandex.ru>
2025-05-13 16:15:43 +0300

    qqq

--- task7/index.php
+++ task7/index.php
@@ -26,10 +26,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       $messages_log[] = 'Спасибо, результаты сохранены.';
       // Если в куках есть пароль, то выводим сообщение.
       if (!empty($_COOKIE['pass'])) {
+        sprintf('Вы можете <a href="login.php">войти</a> с логином <strong>%s</strong>
+          и паролем <strong>%s</strong> для изменения данных.',
+          strip_tags($_COOKIE['login']),
+          strip_tags($_COOKIE['pass']));
         $messages_log[] = sprintf('Вы можете <a href="login.php">войти</a> с логином <strong>%s</strong>
           и паролем <strong>%s</strong> для изменения данных.',
           strip_tags($_COOKIE['login']),
-          strip_tags($_COOKIE['password']));
+          strip_tags($_COOKIE['pass']));
       }
     }
   
